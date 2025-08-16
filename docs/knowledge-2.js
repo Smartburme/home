4a0015a75fb2a051473e407b648aa59a31559336
burmeai.io <athuyrain@gmail.com>
2025-08-16 18:36:00 +0630

    Create knowledge-2.js

---
+++ docs/knowledge-2.js
@@ -0,0 +1,6 @@
+// Knowledge Base 2 - More Questions and Answers
+const knowledge2 = {
+    "what time is it": "I don't have access to the current time.",
+    "tell me a joke": "Why don't scientists trust atoms? Because they make up everything!",
+    "help": "I can answer simple questions. Try asking me about general topics."
+};
