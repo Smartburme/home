916bdce8ca0b238fbae0e0471533e782df7c255e
burmeai.io <athuyrain@gmail.com>
2025-08-16 19:49:22 +0630

    Create coder-knowledge.js

---
+++ knowledge/coder-knowledge.js
@@ -0,0 +1,52 @@
+const coderKnowledgeDB = [
+  {
+    title: "MDN Web Docs",
+    url: "https://developer.mozilla.org/",
+    keywords: ["html", "css", "javascript", "web"]
+  },
+  {
+    title: "Stack Overflow",
+    url: "https://stackoverflow.com/",
+    keywords: ["programming", "help", "debug", "code"]
+  },
+  {
+    title: "GitHub Documentation",
+    url: "https://docs.github.com/",
+    keywords: ["git", "version", "control", "repository"]
+  },
+  {
+    title: "Python Official Docs",
+    url: "https://docs.python.org/3/",
+    keywords: ["python", "scripting", "django", "flask"]
+  },
+  {
+    title: "React Documentation",
+    url: "https://reactjs.org/docs/getting-started.html",
+    keywords: ["react", "frontend", "hooks", "components"]
+  },
+  {
+    title: "Node.js Guides",
+    url: "https://nodejs.org/en/docs/guides/",
+    keywords: ["node", "backend", "server", "javascript"]
+  },
+  {
+    title: "CSS Tricks",
+    url: "https://css-tricks.com/",
+    keywords: ["css", "styles", "layout", "design"]
+  },
+  {
+    title: "Dev.to Community",
+    url: "https://dev.to/",
+    keywords: ["development", "tutorial", "blog", "learning"]
+  },
+  {
+    title: "FreeCodeCamp",
+    url: "https://www.freecodecamp.org/learn/",
+    keywords: ["courses", "free", "certification", "practice"]
+  },
+  {
+    title: "CodePen Challenges",
+    url: "https://codepen.io/challenges/",
+    keywords: ["frontend", "practice", "challenges", "css"]
+  }
+];
