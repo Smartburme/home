470220156d56f07599b4c25e5c89f6f7cb868ab0
burmeai.io <athuyrain@gmail.com>
2025-08-16 19:48:48 +0630

    Create text-knowledge.js

---
+++ knowledge/text-knowledge.js
@@ -0,0 +1,52 @@
+const textKnowledgeDB = [
+  {
+    title: "English Grammar Guide",
+    url: "https://www.grammarly.com/blog/category/grammar/",
+    keywords: ["grammar", "writing", "english", "punctuation"]
+  },
+  {
+    title: "Academic Writing Resources",
+    url: "https://owl.purdue.edu/owl/purdue_owl.html",
+    keywords: ["academic", "writing", "research", "paper"]
+  },
+  {
+    title: "Creative Writing Tips",
+    url: "https://www.masterclass.com/articles/writing-tips",
+    keywords: ["creative", "writing", "story", "fiction"]
+  },
+  {
+    title: "Business Writing Guide",
+    url: "https://www.harvard.edu/writing",
+    keywords: ["business", "professional", "email", "report"]
+  },
+  {
+    title: "Poetry Foundation",
+    url: "https://www.poetryfoundation.org/learn",
+    keywords: ["poetry", "poem", "literature", "verse"]
+  },
+  {
+    title: "Technical Writing Standards",
+    url: "https://developers.google.com/tech-writing",
+    keywords: ["technical", "documentation", "manual", "guide"]
+  },
+  {
+    title: "Copywriting Resources",
+    url: "https://copyblogger.com/blog/",
+    keywords: ["copywriting", "marketing", "advertising", "content"]
+  },
+  {
+    title: "Literary Devices Explained",
+    url: "https://literarydevices.net/",
+    keywords: ["literary", "devices", "metaphor", "simile"]
+  },
+  {
+    title: "Journalism Writing Tips",
+    url: "https://www.poynter.org/educators-students/",
+    keywords: ["journalism", "news", "article", "reporting"]
+  },
+  {
+    title: "Screenwriting Fundamentals",
+    url: "https://screencraft.org/blog/",
+    keywords: ["screenplay", "script", "film", "dialogue"]
+  }
+];
