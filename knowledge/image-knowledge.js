a98e371e07432a68da0516e6fe81d8e0a892990c
burmeai.io <athuyrain@gmail.com>
2025-08-16 19:50:12 +0630

    Create image-knowledge.js

---
+++ knowledge/image-knowledge.js
@@ -0,0 +1,52 @@
+const imageKnowledgeDB = [
+  {
+    title: "Unsplash Free Images",
+    url: "https://unsplash.com/",
+    keywords: ["photos", "free", "stock", "download"]
+  },
+  {
+    title: "Pexels Royalty Free",
+    url: "https://www.pexels.com/",
+    keywords: ["images", "free", "hd", "photography"]
+  },
+  {
+    title: "Pixabay Free Photos",
+    url: "https://pixabay.com/",
+    keywords: ["pictures", "vectors", "illustrations", "free"]
+  },
+  {
+    title: "Adobe Color Wheel",
+    url: "https://color.adobe.com/",
+    keywords: ["color", "palette", "design", "scheme"]
+  },
+  {
+    title: "Canva Design School",
+    url: "https://www.canva.com/learn/",
+    keywords: ["graphic", "design", "tutorial", "template"]
+  },
+  {
+    title: "Figma Design Resources",
+    url: "https://www.figma.com/resources/",
+    keywords: ["ui", "ux", "design", "prototype"]
+  },
+  {
+    title: "Photography Life",
+    url: "https://photographylife.com/",
+    keywords: ["photography", "camera", "lens", "techniques"]
+  },
+  {
+    title: "DPI Calculator",
+    url: "https://www.dpcalc.org/",
+    keywords: ["resolution", "print", "size", "calculator"]
+  },
+  {
+    title: "GIMP Tutorials",
+    url: "https://www.gimp.org/tutorials/",
+    keywords: ["gimp", "photo", "editing", "free"]
+  },
+  {
+    title: "SVG Repo",
+    url: "https://www.svgrepo.com/",
+    keywords: ["svg", "vector", "icons", "graphics"]
+  }
+];
