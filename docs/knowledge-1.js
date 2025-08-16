f858de16f6adeb88bec526dd146b9198e37d4ec7
burmeai.io <athuyrain@gmail.com>
2025-08-16 18:34:22 +0630

    Create knowledge-1.js

---
+++ docs/knowledge-1.js
@@ -0,0 +1,7 @@
+// Knowledge Base 1 - Questions and Answers
+const knowledge1 = {
+    "hello": "Hello! How can I help you today?",
+    "how are you": "I'm just a bot, but thanks for asking!",
+    "what's your name": "I'm a simple chat bot created to help you.",
+    "goodbye": "Goodbye! Have a nice day!"
+};
