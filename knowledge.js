4b8a6c541c9f0b903074b99b4eb3606023fe9d01
burmeai.io <athuyrain@gmail.com>
2025-08-17 19:32:33 +0630

    Update knowledge.js

--- knowledge.js
+++ knowledge.js
@@ -1,30 +1,14 @@
-const Knowledge = {
-    checkBasic: function(message) {
-        // Load knowledge files
-        const basicResponses = Knowledge1.responses;
-        const textResponses = Knowledge2.responses;
-        const imageResponses = Knowledge3.responses;
-        const codeResponses = Knowledge4.responses;
-        
-        // Check all knowledge bases
-        const lowerMsg = message.toLowerCase();
-        
-        if (basicResponses[lowerMsg]) {
-            return basicResponses[lowerMsg];
-        } else if (textResponses[lowerMsg]) {
-            return textResponses[lowerMsg];
-        } else if (imageResponses[lowerMsg]) {
-            return imageResponses[lowerMsg];
-        } else if (codeResponses[lowerMsg]) {
-            return codeResponses[lowerMsg];
-        }
-        
-        return null;
-    }
-};
+import { knowledge1 } from "./docs/answer/knowledge_1.js";
+import { knowledge2 } from "./docs/answer/knowledge_2.js";
+import { knowledge3 } from "./docs/answer/knowledge_3.js";
+import { knowledge4 } from "./docs/answer/knowledge_4.js";
 
-// Load knowledge files
-import { Knowledge1 } from './docs/answer/knowledge_1.js';
-import { Knowledge2 } from './docs/answer/knowledge_2.js';
-import { Knowledge3 } from './docs/answer/knowledge_3.js';
-import { Knowledge4 } from './docs/answer/knowledge_4.js';
+export function handleKnowledge(input) {
+  return (
+    knowledge1(input) ||
+    knowledge2(input) ||
+    knowledge3(input) ||
+    knowledge4(input) ||
+    "🤔 ဒီမေးခွန်းကို မသိသေးပါ။"
+  );
+}
